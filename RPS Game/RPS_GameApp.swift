f88e84cfb2014c28fc1b463640d17d3a57902318
davit <davit.natenadze@gmail.com>
2023-06-02 13:46:47 +0200

    Initial Commit

---
+++ RPS Game/RPS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_GameApp.swift
+//  RPS Game
+//
+//  Created by Davit Natenadze on 02.06.23.
+//
+
+import SwiftUI
+
+@main
+struct RPS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
